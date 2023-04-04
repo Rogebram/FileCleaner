ad1b289773d5bd00c075e3e435f1158a89cf24ef
Roger <57729185+Rogebram@users.noreply.github.com>
2023-04-03 19:24:57 -0600

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,13 +1,13 @@
 import os
 
 # Podemos ingresar la direccion del archivo sin ingresar al codigo
-file_path = input('Ingresa la direccion de tu archivo: ')
+file_path = input('Enter the root of the file: ')
 
 if os.path.isfile(file_path):
     os.remove(file_path)
-    print('El archivo se elimio exitosamente')
+    print('The file was deleted successfuly')
     print(file_path)
 else:
-    print('No se pudo encontrar el archivo')
+    print('File was not found')
     print(file_path)
     
